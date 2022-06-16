8cb4cabff4516120d30818b9e324e677d4289652
LukaszChowanski <chowanski.lukasz@gmail.com>
2022-06-16 23:32:01 +0200

    add missing import

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 import BootstrapContainer from '../SharedComponents/BootstrapContainer'
